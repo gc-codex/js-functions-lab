7565c803fe671f1e774a725cb11e1511f3119b4d
gcrump <gc.crump@icloud.com>
2024-06-29 16:13:25 -0400

    Initial upload of js-function lab.

---
+++ script.js
@@ -0,0 +1,191 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (x, y) => {
+  if (x >= y) {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+
+  /*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+function isAdult(num) {
+ num = 31;
+  if (num >= 21)
+  return ('Adult')
+  else {
+  return ('Minor')
+  }
+}
+
+console.log('Exercise 2 Result:', isAdult(21));
+
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+function isCharAVowel(findVowel) {
+  if (findVowel === 'a' || findVowel === 'e' || findVowel === 'i' || findVowel === 'o' || findVowel === 'u') {
+    return true;
+} else {
+    return false;
+}
+  }
+ 
+isCharAVowel('a', 'e', 'i', 'o', 'u')
+
+
+console.log('Exercise 3 Result:', isCharAVowel("a"));
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+function generateEmail(name, domain) {
+  const email = name + '@' + domain
+  return email 
+
+}
+
+generateEmail('johnsmith', 'example.com')
+
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+function greetUser(name, timeOfDay) {
+
+  const greeting = 'Good ' + timeOfDay + ', ' + name + '! '
+
+  return greeting;
+
+
+
+}
+
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
+
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+function maxOfThree(a, b, c) {
+if (a >= b || a >= c) {
+  return a; 
+
+} else if (b >= a || b >= c) {
+  return b;
+ 
+} else {
+return c; }
+
+}
+
+console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+function calculateTip(a, b) {
+  tip = (b / 100)* a;
+  return tip;
+  
+
+}
+
+console.log('Exercise 7 Result:', calculateTip(50, 20));
+// /*
+// Exercise 8: convertTemperature()
+
+// Write a function named convertTemperature. 
+// It takes two arguments: a temperature and a string representing the 
+// scale ('C' for Celsius, 'F' for Fahrenheit). 
+// Convert the temperature to the other scale.
+
+// Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+// Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+// Complete the exercise in the space below:
+// */
+// function convertTemperature(temp, scale) {
+//   //const conversion = temp + ', ' + scale
+//   return conversion;
+
+// let scaleC = (temp * 1.8) + 32
+// let scaleF = (scale - 32) * .56
+
+
+// if (scale === 'C') {
+//   return scaleC;
+// } else if (scale === 'F') {
+// return scaleF
+// }}
+
+
+// console.log('Exercise 8 Result:', convertTemperature(32, "C"));
+
+
